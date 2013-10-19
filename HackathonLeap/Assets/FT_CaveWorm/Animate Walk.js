901aa12a317dbfbcca7e140a73431ec69defd158
ZakwithoutaC <jakzaeb@gmail.com>
2013-10-19 16:29:23 -0400

    Added Trees, lightning particles, and enemy animations

---
+++ HackathonLeap/Assets/FT_CaveWorm/Animate Walk.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetKeyDown("r")){
+		animation.Play("walk",PlayMode.StopAll);
+		rigidbody.AddForce(0,0,-30);
+	}
+
+}
