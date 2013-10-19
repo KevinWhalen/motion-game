6d6c0082486a7ba0c887c9fc3474e865d8143bb8
Kevin Whalen <kwhalen2@kent.edu>
2013-10-19 11:06:03 -0400

    start of an in-game accent that may not be doing in unity

---
+++ Art/icon-power-level-effect.js
@@ -0,0 +1,68 @@
+/* icon-power-level-effect.js
+ *
+ * A function to control the color fill on the icon accent.
+ * 2013-10-19
+
+electricity
+fire
+water
+wind
+
+*/
+
+// Takes the name of one of the above elements as a string and
+// the power level as an integer percentage (0 to 100).
+function iconFill(element, percent)
+{
+	fileName = element + ".svg";
+	iconId = element + "Icon";
+	layer = iconId + "Layer";
+	shape = iconId + "Shape";
+	accent = iconId + "AccentCresent";
+	document.getElementById(iconId). g g .getElementById('accentCresent'
+
+	var icon : fileName;
+	//var icon : svg;
+
+// http://docs.unity3d.com/Documentation/ScriptReference/GameObject.GetComponent.html
+/*
+	function Start () {
+		var curTransform : Transform;
+		
+		curTransform = gameObject.GetComponent(Transform);
+		// This is equivalent to:
+		curTransform = gameObject.transform;
+	}
+
+	function Update () {
+		// To access public variables and functions 
+		// in another script attached to the same game object.
+		// (ScriptName is the name of the javascript file)
+		var other : ScriptName = gameObject.GetComponent(ScriptName);
+		// Call the function DoSomething on the script
+		other.DoSomething ();
+		// set another variable in the other script instance
+		other.someVariable = 5;
+	}
+
+	// OR
+
+	function Update () {
+		// To access public variables and functions 
+		// in another script attached to the same game object.
+		// (ScriptName is the name of the javascript file)
+		var other : ScriptName;
+		other = gameObject.GetComponent("ScriptName");
+		// Call the function DoSomething on the script
+		other.DoSomething ();
+		// set another variable in the other script instance
+		other.someVariable = 5;
+	}
+*/
+
+
+// http://wiki.unity3d.com/index.php?title=SVG&action=edit
+
+// http://virtualplayground.d2.pl/?p=241
+
+}
